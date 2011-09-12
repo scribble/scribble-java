35ce32087301700ea996d14718bbd2bafb8c656d
objectiser <gary@pi4tech.com>
2011-09-12 08:54:44 +0100

    SCRIBBLE-116 further tests

--- bundles/org.scribble.protocol/src/test/java/org/scribble/protocol/validation/rules/ProtocolValidatorRuleTest.java
+++ bundles/org.scribble.protocol/src/test/java/org/scribble/protocol/validation/rules/ProtocolValidatorRuleTest.java
@@ -22,6 +22,75 @@ import org.scribble.protocol.model.*;
 public class ProtocolValidatorRuleTest {
 
 	@org.junit.Test
+	public void testUnconnectedIntroduces() {
+		Protocol p=new Protocol();
+		ParameterDefinition pd=new ParameterDefinition();
+		pd.setName("A");
+		p.getParameterDefinitions().add(pd);
+		
+		Role roleB=new Role("B");
+		Role roleC=new Role("C");
+		
+		Introduces intros=new Introduces();
+		intros.setIntroducer(roleB);
+		intros.getRoles().add(roleC);
+		p.getBlock().add(intros);
+		
+		TestScribbleLogger logger=new TestScribbleLogger();
+
+		ProtocolValidatorRule rule=new ProtocolValidatorRule();
+		rule.validate(null, p, logger);
+		
+		logger.verifyErrors(new String[]{
+				MessageFormat.format(
+						java.util.PropertyResourceBundle.getBundle(
+						"org.scribble.protocol.Messages").getString("_UNCONNECTED_ROLE"),
+						roleB.getName())
+		});
+	}
+
+	@org.junit.Test
+	public void testConnectedSimpleSequence() {
+		Protocol p=new Protocol();
+		ParameterDefinition pd=new ParameterDefinition();
+		pd.setName("A");
+		p.getParameterDefinitions().add(pd);
+		
+		Role roleA=new Role("A");
+		Role roleB=new Role("B");
+		Role roleC=new Role("C");
+		Role roleD=new Role("D");
+		
+		Introduces intros=new Introduces();
+		intros.setIntroducer(roleA);
+		intros.getRoles().add(roleB);
+		p.getBlock().add(intros);
+		
+		Interaction i1=new Interaction();
+		i1.setFromRole(roleA);
+		i1.getToRoles().add(roleB);
+		p.getBlock().add(i1);
+		
+		Interaction i2=new Interaction();
+		i2.setFromRole(roleB);
+		i2.getToRoles().add(roleC);
+		p.getBlock().add(i2);
+		
+		Interaction i3=new Interaction();
+		i3.setFromRole(roleC);
+		i3.getToRoles().add(roleD);
+		p.getBlock().add(i3);
+		
+		TestScribbleLogger logger=new TestScribbleLogger();
+
+		ProtocolValidatorRule rule=new ProtocolValidatorRule();
+		rule.validate(null, p, logger);
+		
+		logger.verifyErrors(new String[]{
+		});
+	}
+
+	@org.junit.Test
 	public void testUnconnectedSimpleSequence() {
 		Protocol p=new Protocol();
 		ParameterDefinition pd=new ParameterDefinition();
@@ -77,6 +146,50 @@ public class ProtocolValidatorRuleTest {
 		intros1.getRoles().add(roleB);
 		p.getBlock().add(intros1);
 		
+		Interaction i1=new Interaction();
+		i1.setFromRole(roleA);
+		i1.getToRoles().add(roleB);
+		p.getBlock().add(i1);
+		
+		Choice choice=new Choice();
+		choice.setRole(roleC);
+		p.getBlock().add(choice);
+		
+		Block b1=new Block();
+		choice.getBlocks().add(b1);
+		
+		Block b2=new Block();
+		choice.getBlocks().add(b2);
+		
+		TestScribbleLogger logger=new TestScribbleLogger();
+
+		ProtocolValidatorRule rule=new ProtocolValidatorRule();
+		rule.validate(null, p, logger);
+		
+		logger.verifyErrors(new String[]{
+				MessageFormat.format(
+						java.util.PropertyResourceBundle.getBundle(
+						"org.scribble.protocol.Messages").getString("_UNCONNECTED_ROLE"),
+						roleC.getName())
+		});
+	}
+
+	@org.junit.Test
+	public void testUnconnectedChoicePath() {
+		Protocol p=new Protocol();
+		ParameterDefinition pd=new ParameterDefinition();
+		pd.setName("A");
+		p.getParameterDefinitions().add(pd);
+		
+		Role roleA=new Role("A");
+		Role roleB=new Role("B");
+		Role roleC=new Role("C");
+		
+		Introduces intros1=new Introduces();
+		intros1.setIntroducer(roleA);
+		intros1.getRoles().add(roleB);
+		p.getBlock().add(intros1);
+		
 		Introduces intros2=new Introduces();
 		intros2.setIntroducer(roleB);
 		intros2.getRoles().add(roleC);
@@ -109,6 +222,242 @@ public class ProtocolValidatorRuleTest {
 						java.util.PropertyResourceBundle.getBundle(
 						"org.scribble.protocol.Messages").getString("_UNCONNECTED_ROLE"),
 						roleB.getName())
+		});
+	}
+
+	@org.junit.Test
+	public void testConnectedChoicePath() {
+		Protocol p=new Protocol();
+		ParameterDefinition pd=new ParameterDefinition();
+		pd.setName("A");
+		p.getParameterDefinitions().add(pd);
+		
+		Role roleA=new Role("A");
+		Role roleB=new Role("B");
+		Role roleC=new Role("C");
+		
+		Introduces intros1=new Introduces();
+		intros1.setIntroducer(roleA);
+		intros1.getRoles().add(roleB);
+		p.getBlock().add(intros1);
+		
+		Introduces intros2=new Introduces();
+		intros2.setIntroducer(roleB);
+		intros2.getRoles().add(roleC);
+		p.getBlock().add(intros2);
+		
+		Interaction i1=new Interaction();
+		i1.setFromRole(roleA);
+		i1.getToRoles().add(roleB);
+		p.getBlock().add(i1);
+		
+		// Make choice at B instead
+		Choice choice=new Choice();
+		choice.setRole(roleB);
+		p.getBlock().add(choice);
+		
+		Block b1=new Block();
+		choice.getBlocks().add(b1);
+		
+		Interaction i2=new Interaction();
+		i2.setFromRole(roleB);
+		i2.getToRoles().add(roleC);
+		b1.add(i2);
+		
+		TestScribbleLogger logger=new TestScribbleLogger();
+
+		ProtocolValidatorRule rule=new ProtocolValidatorRule();
+		rule.validate(null, p, logger);
+		
+		logger.verifyErrors(new String[]{
+		});
+	}
+
+	@org.junit.Test
+	public void testUnconnectedRepeat() {
+		Protocol p=new Protocol();
+		ParameterDefinition pd=new ParameterDefinition();
+		pd.setName("A");
+		p.getParameterDefinitions().add(pd);
+		
+		Role roleA=new Role("A");
+		Role roleB=new Role("B");
+		Role roleC=new Role("C");
+		
+		Introduces intros1=new Introduces();
+		intros1.setIntroducer(roleA);
+		intros1.getRoles().add(roleB);
+		p.getBlock().add(intros1);
+		
+		Interaction i1=new Interaction();
+		i1.setFromRole(roleA);
+		i1.getToRoles().add(roleB);
+		p.getBlock().add(i1);
+		
+		Repeat repeat=new Repeat();
+		repeat.getRoles().add(roleC);
+		p.getBlock().add(repeat);
+		
+		TestScribbleLogger logger=new TestScribbleLogger();
+
+		ProtocolValidatorRule rule=new ProtocolValidatorRule();
+		rule.validate(null, p, logger);
+		
+		logger.verifyErrors(new String[]{
+				MessageFormat.format(
+						java.util.PropertyResourceBundle.getBundle(
+						"org.scribble.protocol.Messages").getString("_UNCONNECTED_ROLE"),
+						roleC.getName())
+		});
+	}
+
+	@org.junit.Test
+	public void testUnconnectedRepeatBody() {
+		Protocol p=new Protocol();
+		ParameterDefinition pd=new ParameterDefinition();
+		pd.setName("A");
+		p.getParameterDefinitions().add(pd);
+		
+		Role roleA=new Role("A");
+		Role roleB=new Role("B");
+		Role roleC=new Role("C");
+		
+		Introduces intros1=new Introduces();
+		intros1.setIntroducer(roleA);
+		intros1.getRoles().add(roleB);
+		p.getBlock().add(intros1);
+		
+		Introduces intros2=new Introduces();
+		intros2.setIntroducer(roleB);
+		intros2.getRoles().add(roleC);
+		p.getBlock().add(intros2);
+		
+		Interaction i1=new Interaction();
+		i1.setFromRole(roleA);
+		i1.getToRoles().add(roleB);
+		p.getBlock().add(i1);
+		
+		Repeat repeat=new Repeat();
+		repeat.getRoles().add(roleA);
+		p.getBlock().add(repeat);
+		
+		Interaction i2=new Interaction();
+		i2.setFromRole(roleB);
+		i2.getToRoles().add(roleC);
+		repeat.getBlock().add(i2);
+		
+		TestScribbleLogger logger=new TestScribbleLogger();
+
+		ProtocolValidatorRule rule=new ProtocolValidatorRule();
+		rule.validate(null, p, logger);
+		
+		logger.verifyErrors(new String[]{
+				MessageFormat.format(
+						java.util.PropertyResourceBundle.getBundle(
+						"org.scribble.protocol.Messages").getString("_UNCONNECTED_ROLE"),
+						roleB.getName())
+		});
+	}
+
+	@org.junit.Test
+	public void testConnectedRepeatBody() {
+		Protocol p=new Protocol();
+		ParameterDefinition pd=new ParameterDefinition();
+		pd.setName("A");
+		p.getParameterDefinitions().add(pd);
+		
+		Role roleA=new Role("A");
+		Role roleB=new Role("B");
+		Role roleC=new Role("C");
+		
+		Introduces intros1=new Introduces();
+		intros1.setIntroducer(roleA);
+		intros1.getRoles().add(roleB);
+		p.getBlock().add(intros1);
+		
+		Introduces intros2=new Introduces();
+		intros2.setIntroducer(roleB);
+		intros2.getRoles().add(roleC);
+		p.getBlock().add(intros2);
+		
+		Interaction i1=new Interaction();
+		i1.setFromRole(roleA);
+		i1.getToRoles().add(roleB);
+		p.getBlock().add(i1);
+		
+		// Make decision at B instead
+		Repeat repeat=new Repeat();
+		repeat.getRoles().add(roleB);
+		p.getBlock().add(repeat);
+		
+		Interaction i2=new Interaction();
+		i2.setFromRole(roleB);
+		i2.getToRoles().add(roleC);
+		repeat.getBlock().add(i2);
+		
+		TestScribbleLogger logger=new TestScribbleLogger();
+
+		ProtocolValidatorRule rule=new ProtocolValidatorRule();
+		rule.validate(null, p, logger);
+		
+		logger.verifyErrors(new String[]{
+		});
+	}
+
+	@org.junit.Test
+	public void testUnconnectedParallel() {
+		Protocol p=new Protocol();
+		ParameterDefinition pd=new ParameterDefinition();
+		pd.setName("A");
+		p.getParameterDefinitions().add(pd);
+		
+		Role roleA=new Role("A");
+		Role roleB=new Role("B");
+		Role roleC=new Role("C");
+		Role roleD=new Role("D");
+		
+		Introduces intros1=new Introduces();
+		intros1.setIntroducer(roleA);
+		intros1.getRoles().add(roleB);
+		p.getBlock().add(intros1);
+		
+		Interaction i1=new Interaction();
+		i1.setFromRole(roleA);
+		i1.getToRoles().add(roleB);
+		p.getBlock().add(i1);
+		
+		Parallel parallel=new Parallel();
+		p.getBlock().add(parallel);
+		
+		Block b1=new Block();
+		parallel.getBlocks().add(b1);
+		
+		Block b2=new Block();
+		parallel.getBlocks().add(b2);
+		
+		Interaction i2=new Interaction();
+		i2.setFromRole(roleB);
+		i2.getToRoles().add(roleC);
+		b1.add(i2);
+		
+		// Not connected, because this concurrent path is not connected to
+		// the interaction sentences preceding the parallel - the other parallel
+		// paths don't count
+		Interaction i3=new Interaction();
+		i3.setFromRole(roleC);
+		i3.getToRoles().add(roleD);
+		b2.add(i3);
+		
+		TestScribbleLogger logger=new TestScribbleLogger();
+
+		ProtocolValidatorRule rule=new ProtocolValidatorRule();
+		rule.validate(null, p, logger);
+		
+		logger.verifyErrors(new String[]{
+				MessageFormat.format(
+						java.util.PropertyResourceBundle.getBundle(
+						"org.scribble.protocol.Messages").getString("_UNCONNECTED_ROLE"),
+						roleC.getName())
 		});
 	}
 }
