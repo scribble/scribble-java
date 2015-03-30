61493ebbb5b3f6c1098324cccbf40d67209f1982
Ray Hu <raymond.hu05@imperial.ac.uk>
2015-03-30 21:09:42 +0100

    Continued on projection pass.

---
+++ modules/core/src/main/java/org/scribble2/model/del/global/GlobalContinueDelegate.java
@@ -0,0 +1,26 @@
+package org.scribble2.model.del.global;
+
+import org.scribble2.model.ModelFactoryImpl;
+import org.scribble2.model.ModelNode;
+import org.scribble2.model.del.SimpleInteractionNodeDelegate;
+import org.scribble2.model.global.GlobalContinue;
+import org.scribble2.model.local.LocalContinue;
+import org.scribble2.model.name.simple.RecursionVarNode;
+import org.scribble2.model.visit.Projector;
+import org.scribble2.model.visit.env.ProjectionEnv;
+import org.scribble2.util.ScribbleException;
+
+// FIXME: make base MessageTransferDelegate?
+public class GlobalContinueDelegate extends SimpleInteractionNodeDelegate
+{
+	@Override
+	public GlobalContinue leaveProjection(ModelNode parent, ModelNode child, Projector proj, ModelNode visited) throws ScribbleException //throws ScribbleException
+	{
+		GlobalContinue gc = (GlobalContinue) visited;
+		RecursionVarNode recvar = new RecursionVarNode(gc.recvar.toName().toString());
+		LocalContinue projection = ModelFactoryImpl.FACTORY.LocalContinue(recvar);
+		ProjectionEnv env = proj.popEnv();
+		proj.pushEnv(new ProjectionEnv(env.getJobContext(), env.getModuleDelegate(), projection));
+		return (GlobalContinue) super.leaveProjection(parent, child, proj, gc);  // records the current checker Env to the current del; also pops and merges that env into the parent env
+	}
+}
