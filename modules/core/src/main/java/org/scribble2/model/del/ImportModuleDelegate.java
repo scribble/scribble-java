81fbc62d436e732e1aa8fcc8b589e2608da2d354
Ray Hu <raymond.hu05@imperial.ac.uk>
2015-04-14 20:39:41 +0100

    Added missing file.

---
+++ modules/core/src/main/java/org/scribble2/model/del/ImportModuleDelegate.java
@@ -0,0 +1,44 @@
+package org.scribble2.model.del;
+
+import org.scribble2.model.ModelNode;
+import org.scribble2.model.Module;
+import org.scribble2.model.visit.Projector;
+
+public class ImportModuleDelegate extends ModelDelegateBase
+{
+	public ImportModuleDelegate()
+	{
+
+	}
+
+	/*Handled up front in ModuleContext
+	@Override
+	//public ContextBuilder enterContextBuilding(ModelNode parent, ModelNode child, ContextBuilder builder)
+	public void enterContextBuilding(ModelNode parent, ModelNode child, ContextBuilder builder)
+	{
+		//builder.setModuleContext(new ModuleContext(builder.job.getJobContext(), (Module) child));
+		builder.setModuleContext(new ModuleContext((Module) child));
+		//return builder;
+	}
+
+	// Maybe better to create on enter, so can be used during the context build pass (Context would need to be "cached" in the visitor to be accessed)
+	@Override
+	public ModelNode leaveContextBuilding(ModelNode parent, ModelNode child, ContextBuilder builder, ModelNode visited) throws ScribbleException
+	{
+		//visited = visited.del(new ModuleDelegate(builder.getJobContext(), (Module) visited));
+		ImportModuleDelegate del = copy();  // FIXME: should be a deep clone in principle
+		//del.context = new ModuleContext(builder.getJobContext(), (Module) visited);
+		del.context = builder.getModuleContext();
+		return visited.del(del);
+	}*/
+
+	@Override
+	public Module leaveProjection(ModelNode parent, ModelNode child, Projector proj, ModelNode visited) //throws ScribbleException
+	{
+		/*// Don't know target protocol
+		PrimitiveNameNode pnn = new PrimitiveNameNode(null, proj.getProjectedModuleName(this.fmn.smn, ...));
+		ModuleNameNode mnn = new ModuleNameNode(this.fmn.pn, pnn);
+		return new ImportModule(null, mnn, this.alias);*/
+		throw new RuntimeException("TODO");
+	}
+}
