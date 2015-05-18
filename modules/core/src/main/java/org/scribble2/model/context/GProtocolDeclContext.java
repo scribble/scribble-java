1479c292eb7649a93bb682e491ea49628071923b
Ray Hu <raymond.hu05@imperial.ac.uk>
2015-05-18 21:49:28 +0100

    Refactoring global/local protocol dependency building.

---
+++ modules/core/src/main/java/org/scribble2/model/context/GProtocolDeclContext.java
@@ -0,0 +1,43 @@
+package org.scribble2.model.context;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.scribble2.sesstype.kind.Global;
+import org.scribble2.sesstype.name.GProtocolName;
+import org.scribble2.sesstype.name.ProtocolName;
+import org.scribble2.sesstype.name.Role;
+
+// Make abstract with Global/Local extensions?
+//public class GProtocolDeclContext extends ProtocolDeclContext<GProtocolName, Global>
+public class GProtocolDeclContext extends ProtocolDeclContext<Global>
+{
+	// cache of dependencies, cleared on entering each root global protocol
+	// protocol name is full name of global protocol dependencies
+	//private Map<Role, Map<GProtocolName, Set<Role>>> dependencies;  // All the potential dependencies from this protocol decl as the root
+			// FIXME: generalise to support locals
+	
+	//public GProtocolDeclContext(Map<Role, Map<? extends ProtocolName<Global>, Set<Role>>> dependencies)
+	public GProtocolDeclContext(Map<Role, Map<GProtocolName, Set<Role>>> dependencies)
+	{
+		//this.dependencies = dependencies;
+		super(cast(dependencies));
+	}
+	
+	private static Map<Role, Map<? extends ProtocolName<Global>, Set<Role>>> cast(Map<Role, Map<GProtocolName, Set<Role>>> map)
+	{
+		return map.keySet().stream().collect(Collectors.toMap((r) -> r, (r) -> castAux(map.get(r))));
+	}
+
+	private static Map<? extends ProtocolName<Global>, Set<Role>> castAux(Map<GProtocolName, Set<Role>> map)
+	{
+		return map.keySet().stream().collect(Collectors.toMap((k) -> k, (k) -> map.get(k)));
+	}
+	
+	/*@Override
+	public Map<Role, Map<GProtocolName, Set<Role>>> getDependencies()
+	{
+		return this.dependencies;
+	}*/
+}
---
+++ modules/core/src/main/java/org/scribble2/model/context/LProtocolDeclContext.java
@@ -0,0 +1,45 @@
+package org.scribble2.model.context;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.scribble2.sesstype.kind.Local;
+import org.scribble2.sesstype.name.LProtocolName;
+import org.scribble2.sesstype.name.ProtocolName;
+import org.scribble2.sesstype.name.Role;
+
+// Make abstract with Global/Local extensions?
+//public class LProtocolDeclContext extends ProtocolDeclContext<LProtocolName, Local>
+public class LProtocolDeclContext extends ProtocolDeclContext<Local>
+{
+	// cache of dependencies, cleared on entering each root global protocol
+	// protocol name is full name of global protocol dependencies
+	//private Map<Role, Map<GProtocolName, Set<Role>>> dependencies;  // All the potential dependencies from this protocol decl as the root
+			// FIXME: generalise to support locals
+	
+	public LProtocolDeclContext(Map<Role, Map<LProtocolName, Set<Role>>> dependencies)
+	//public LProtocolDeclContext(Map<Role, Map<? extends ProtocolName<Local>, Set<Role>>> dependencies)
+	//public LProtocolDeclContext(Map<Role, Map<LProtocolName, Set<Role>>> dependencies)
+	{
+		//this.dependencies = dependencies;
+		super(cast(dependencies));
+	}
+	
+	private static Map<Role, Map<? extends ProtocolName<Local>, Set<Role>>> cast(Map<Role, Map<LProtocolName, Set<Role>>> map)
+	{
+		return map.keySet().stream().collect(Collectors.toMap((r) -> r, (r) -> castAux(map.get(r))));
+	}
+
+	private static Map<? extends ProtocolName<Local>, Set<Role>> castAux(Map<LProtocolName, Set<Role>> map)
+	{
+		return map.keySet().stream().collect(Collectors.toMap((k) -> k, (k) -> map.get(k)));
+	}
+	
+	
+	/*@Override
+	public Map<Role, Map<GProtocolName, Set<Role>>> getDependencies()
+	{
+		return this.dependencies;
+	}*/
+}
