72982de8eb872f95285c1f0886731564215f0221
Ray Hu <raymond.hu05@imperial.ac.uk>
2016-05-31 20:57:16 +0100

    Minor.

--- modules/core/src/main/java/org/scribble/visit/GlobalModelChecker.java
+++ modules/core/src/main/java/org/scribble/visit/GlobalModelChecker.java
@@ -8,7 +8,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.scribble.ast.Module;
 import org.scribble.ast.ProtocolDecl;
