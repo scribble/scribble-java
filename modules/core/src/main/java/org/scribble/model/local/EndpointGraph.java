d3fa682ba500694f1c6052da620528993ceaeeea
Ray Hu <raymond.hu05@imperial.ac.uk>
2016-06-06 22:44:48 +0100

    Minor.

--- modules/core/src/main/java/org/scribble/model/local/EndpointGraph.java
+++ modules/core/src/main/java/org/scribble/model/local/EndpointGraph.java
@@ -23,7 +23,7 @@ public class EndpointGraph
 		return this.init.toDot();
 	}
 	
-	.. move back to endpointstate
+	/*.. move back to endpointstate
 	.. use getallreachable to get subgraph, make a graph clone method
 	.. for each poly output, clone a (non-det) edge to clone of the reachable subgraph with the clone of the current node pruned to this single choice
 	..     be careful of original non-det edges, need to do each separately
@@ -31,7 +31,7 @@ public class EndpointGraph
 	.. is it equiv to requiring all roles to see every choice path?  except initial accepting roles -- yes
 	.. easier to implement as a direct check on the standard global model, rather than model hacking -- i.e. liveness is not just about terminal sets, but about "branching condition", c.f. julien?
 	.. the issue is connect/accept -- makes direct check a bit more complicated, maybe value in doing it by model hacking to rely on standard liveness checking?
-	..     should be fine, check set of roles on each path is equal, except for accept-guarded initial roles
+	..     should be fine, check set of roles on each path is equal, except for accept-guarded initial roles*/
 	public static EndpointGraph getOutputSubtypes(EndpointGraph g)
 	{
 		LGraphBuilder builder = new LGraphBuilder();
