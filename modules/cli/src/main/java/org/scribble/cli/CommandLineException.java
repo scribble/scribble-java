0e2cd3345c2a71509a1cb5af5a244c26a0d62315
Ray Hu <raymond.hu05@imperial.ac.uk>
2016-06-08 10:54:12 +0100

    Added missing CommandLineException class.

---
+++ modules/cli/src/main/java/org/scribble/cli/CommandLineException.java
@@ -0,0 +1,33 @@
+package org.scribble.cli;
+
+public class CommandLineException extends Exception
+{
+	public CommandLineException()
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public CommandLineException(String arg0)
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CommandLineException(Throwable arg0)
+	{
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CommandLineException(String arg0, Throwable arg1)
+	{
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CommandLineException(String arg0, Throwable arg1, boolean arg2, boolean arg3)
+	{
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+}
