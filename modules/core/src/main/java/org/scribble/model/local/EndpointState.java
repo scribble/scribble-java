31b8c98f3216bbe1711f6f01e2acf784f37b8173
Ray Hu <raymond.hu05@imperial.ac.uk>
2016-06-09 22:11:07 +0100

    Minor.

--- modules/core/src/main/java/org/scribble/model/local/EndpointState.java
+++ modules/core/src/main/java/org/scribble/model/local/EndpointState.java
@@ -8,7 +8,6 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 
 import org.scribble.main.ScribbleException;
