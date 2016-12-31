c50701df00beb8b67dc25e744900eed79abb5423
Ray Hu <raymond.hu05@imperial.ac.uk>
2016-12-31 16:35:25 +0000

    Extended f17 projection.

--- modules/f17/src/main/java/org/scribble/ext/f17/ast/local/F17Projector.java
+++ modules/f17/src/main/java/org/scribble/ext/f17/ast/local/F17Projector.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.scribble.ext.f17.ast.F17AstFactory;
 import org.scribble.ext.f17.ast.global.F17GChoice;
@@ -56,17 +57,17 @@ public class F17Projector
 			}
 			else // gc.cases.size() > 1
 			{
-				Map<F17LAction, F17LType> pCases = new HashMap<>();
+				Map<F17LAction, F17LType> aCases = new HashMap<>();
 				Map<F17GAction, RecVar> rvCases = new HashMap<>();
 				Set<F17GAction> eCases = new HashSet<>();
-
 				for (Entry<F17GAction, F17GType> e : gc.cases.entrySet())
 				{
+					// Could be partly factored out with above unary choice case (e.g., make an intermediate unary choice for recursive call)
 					F17GAction ga = e.getKey();
 					if (ga.getRoles().contains(r))
 					{
 						F17LType lt = project(e.getValue(), r, Collections.emptySet());
-						pCases.put(project(e.getKey(), r), lt);
+						aCases.put(project(e.getKey(), r), lt);
 					}
 					else
 					{
@@ -82,55 +83,36 @@ public class F17Projector
 						}
 						else
 						{
+
+							/*// Original
 							if (!(lt instanceof F17LChoice) || ((F17LChoice) lt).cases.size() > 1)  // FIXME: generalise >1 cases
 							{
 								throw new F17Exception("[f17] Not projectable (non prefix-guarded case) onto " + r + ": " + lt);
 							}
 							Entry<F17LAction, F17LType> tmp = ((F17LChoice) lt).cases.entrySet().iterator().next();
-							pCases.put(tmp.getKey(), tmp.getValue());
+							aCases.put(tmp.getKey(), tmp.getValue());*/
+							
+							// Extended
+							if (lt instanceof F17LRec)
+							{
+								lt = ((F17LRec) lt).unfold();
+							}
+							if (!(lt instanceof F17LChoice))
+							{
+								throw new F17Exception("[f17] Not projectable (non prefix-guarded case) onto " + r + ": " + lt);  // Shouldn't get in here?
+							}
+							for (Entry<F17LAction, F17LType> tmp : ((F17LChoice) lt).cases.entrySet())  // Nested choice flattening
+							{
+								aCases.put(tmp.getKey(), tmp.getValue());
+							}
+
 						}
 					}
 				}
 
-				if (!rvCases.isEmpty() && pCases.isEmpty() && eCases.isEmpty() && new HashSet<>(rvCases.values()).size() == 1)
-				{
-					return this.factory.LRecVar(rvCases.values().iterator().next());
-				}
-				else if (!eCases.isEmpty() && pCases.isEmpty() && rvCases.isEmpty())
-				{
-					return this.factory.LEnd();
-				}
-				else
-				{
-					for (RecVar rv : rvCases.values())
-					{
-						if (!delta.contains(rv))
-						{
-							throw new F17Exception("[f17] Not projectable (unguarded rec case) onto " + r + ": " + rv);
-						}
-					}
-					F17LAction firsta = pCases.keySet().iterator().next();
-					if (firsta.isOutput())
-					{
-						if (pCases.keySet().stream().anyMatch((k) -> !k.isOutput()))
-						{
-							throw new F17Exception("[f17] Inconsistent choice: " + gc);
-						}
-					}
-					else  // firsta.isInput()
-					{
-						if (pCases.keySet().stream().anyMatch((k) -> !k.isInput()))
-						{
-							throw new F17Exception("[f17] Inconsistent choice: " + gc);
-						}
-						Role peer = ((F17LInput) firsta).peer;
-						if (pCases.keySet().stream().anyMatch((k) -> !((F17LInput) k).peer.equals(peer)))
-						{
-							throw new F17Exception("[f17] Inconsistent input choice subjects: " + gc);  // subject means global action subjs (although also means peer in local)
-						}
-					}
-					return this.factory.LChoice(pCases);
-				}
+				return
+							//orig(gc, r, delta, pCases, rvCases, eCases);
+							extended(gc, r, delta, aCases, rvCases, eCases);
 			}
 		}
 		else if (gt instanceof F17GRec)
@@ -139,7 +121,7 @@ public class F17Projector
 			Set<RecVar> delta1 = new HashSet<>(delta);
 			delta1.add(gr.recvar);
 			F17LType lt = project(gr.body, r, delta1);
-			if (lt instanceof F17LRecVar)
+			if (lt instanceof F17LRecVar || lt instanceof F17LEnd)  //** end case
 			{
 				return this.factory.LEnd();
 			}
@@ -157,6 +139,107 @@ public class F17Projector
 		else
 		{
 			throw new RuntimeException("[f17] Shouldn't get in here: " + gt);
+		}
+	}
+
+	private F17LType extended(F17GChoice gc, Role r, Set<RecVar> delta,
+			Map<F17LAction, F17LType> pCases, Map<F17GAction, RecVar> rvCases, Set<F17GAction> eCases) throws F17Exception
+	{
+		//return null;
+		rvCases = rvCases.entrySet().stream().filter((e) -> !delta.contains(e.getValue()))
+				.collect(Collectors.toMap((e) -> e.getKey(), (e) -> e.getValue()));
+
+		if (pCases.isEmpty())
+		{
+			if (rvCases.isEmpty())
+			{
+				return this.factory.LEnd();
+			}
+			else
+			{
+				RecVar rv = rvCases.values().iterator().next();
+				if (rvCases.values().stream().allMatch((v) -> v.equals(rv)))
+				{
+					return this.factory.LRecVar(rvCases.values().iterator().next());
+				}
+				else
+				{
+					throw new F17Exception("[f17] Cannot \"merge\" in projection onto " + r + ": " + rvCases.values());  // FIXME: src blame
+				}
+			}
+		}
+		else
+		{
+			if (!rvCases.isEmpty())
+			{
+				throw new F17Exception("[f17] Cannot project onto " + r + ": " + gc);  // FIXME: src blame
+			}
+			// Following same ax "orig"
+			F17LAction firsta = pCases.keySet().iterator().next();
+			if (firsta.isOutput())  // Non- mixed-role choice subsumed by role enabling?
+			{
+				if (pCases.keySet().stream().anyMatch((k) -> !k.isOutput()))
+				{
+					throw new F17Exception("[f17] Inconsistent choice: " + gc);
+				}
+			}
+			else  // firsta.isInput()
+			{
+				if (pCases.keySet().stream().anyMatch((k) -> !k.isInput()))
+				{
+					throw new F17Exception("[f17] Inconsistent choice: " + gc);
+				}
+				Role peer = ((F17LInput) firsta).peer;
+				if (pCases.keySet().stream().anyMatch((k) -> !((F17LInput) k).peer.equals(peer)))
+				{
+					throw new F17Exception("[f17] Inconsistent input choice subjects: " + gc);  // subject means global action subjs (although also means peer in local)
+				}
+			}
+			return this.factory.LChoice(pCases);
+		}
+	}
+
+	private F17LType orig(F17GChoice gc, Role r, Set<RecVar> delta,
+			Map<F17LAction, F17LType> pCases, Map<F17GAction, RecVar> rvCases, Set<F17GAction> eCases) throws F17Exception
+	{
+		if (!rvCases.isEmpty() && pCases.isEmpty() && eCases.isEmpty() && new HashSet<>(rvCases.values()).size() == 1)
+		{
+			return this.factory.LRecVar(rvCases.values().iterator().next());
+		}
+		else if (!eCases.isEmpty() && pCases.isEmpty() && rvCases.isEmpty())
+		{
+			return this.factory.LEnd();
+		}
+		else
+		{
+			for (RecVar rv : rvCases.values())
+			{
+				if (!delta.contains(rv))
+				{
+					throw new F17Exception("[f17] Not projectable (unguarded rec case) onto " + r + ": " + rv);
+				}
+			}
+			F17LAction firsta = pCases.keySet().iterator().next();
+			if (firsta.isOutput())  // Non- mixed-role choice subsumed by role enabling?
+			{
+				if (pCases.keySet().stream().anyMatch((k) -> !k.isOutput()))
+				{
+					throw new F17Exception("[f17] Inconsistent choice: " + gc);
+				}
+			}
+			else  // firsta.isInput()
+			{
+				if (pCases.keySet().stream().anyMatch((k) -> !k.isInput()))
+				{
+					throw new F17Exception("[f17] Inconsistent choice: " + gc);
+				}
+				Role peer = ((F17LInput) firsta).peer;
+				if (pCases.keySet().stream().anyMatch((k) -> !((F17LInput) k).peer.equals(peer)))
+				{
+					throw new F17Exception("[f17] Inconsistent input choice subjects: " + gc);  // subject means global action subjs (although also means peer in local)
+				}
+			}
+			return this.factory.LChoice(pCases);
 		}
 	}
 
