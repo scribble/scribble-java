33de5a075f0639bef34efe8aed4763462d5341a9
Ray Hu <raymond.hu@imperial.ac.uk>
2017-09-07 17:47:03 +0100

    Minor fix to param-core API gen.

--- scribble-go/src/main/java/org/scribble/ext/go/core/codegen/statetype/ParamCoreSTSendActionBuilder.java
+++ scribble-go/src/main/java/org/scribble/ext/go/core/codegen/statetype/ParamCoreSTSendActionBuilder.java
@@ -60,7 +60,7 @@ public class ParamCoreSTSendActionBuilder extends STSendActionBuilder
 			else if (e instanceof ParamIndexVar)
 			{
 				return ParamCoreSTApiGenConstants.GO_IO_FUN_RECEIVER + "."
-					+ ParamCoreSTApiGenConstants.GO_SCHAN_ENDPOINT + ".params[" + e + "]";
+					+ ParamCoreSTApiGenConstants.GO_SCHAN_ENDPOINT + ".params[\"" + e + "\"]";
 			}
 			else
 			{
