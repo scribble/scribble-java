9bf5471991e64d73feb7a66d50ee1d5e400312fa
Ray Hu <raymond.hu@imperial.ac.uk>
2017-09-07 21:38:30 +0100

    Added param-core API gen for branching.

--- scribble-go/src/main/java/org/scribble/ext/go/core/codegen/statetype/ParamCoreSTBranchActionBuilder.java
+++ scribble-go/src/main/java/org/scribble/ext/go/core/codegen/statetype/ParamCoreSTBranchActionBuilder.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.scribble.codegen.statetype.STReceiveActionBuilder;
+import org.scribble.codegen.statetype.STBranchActionBuilder;
 import org.scribble.codegen.statetype.STStateChanApiBuilder;
 import org.scribble.ext.go.core.model.endpoint.action.ParamCoreEAction;
 import org.scribble.ext.go.core.type.ParamRange;
@@ -15,8 +15,20 @@ import org.scribble.ext.go.type.index.ParamIndexVar;
 import org.scribble.model.endpoint.EState;
 import org.scribble.model.endpoint.actions.EAction;
 
-public class ParamCoreSTReceiveActionBuilder extends STReceiveActionBuilder
+public class ParamCoreSTBranchActionBuilder extends STBranchActionBuilder
 {
+	/*@Override
+	public String build(STStateChanApiBuilder api, EState curr, EAction a)  // FIXME: "overriding" GSTStateChanAPIBuilder.buildAction to hack around *interface return  // FIXME: factor out
+	{
+		EState succ = curr.getSuccessor(a); 
+		return
+				  "func (s *" + getStateChanType(api, curr, a) + ") " + getActionName(api, a) + "(" 
+				+ buildArgs(a)
+				+ ") " + getReturnType(api, curr, succ) + " {\n"  // HACK: Return type is interface, so no need for *return (unlike other state chans)
+				+ "s.state.Use()\n"
+				+ buildBody(api, curr, a, succ) + "\n"
+				+ "}";
+	}*/
 
 	@Override
 	public String getActionName(STStateChanApiBuilder api, EAction a)
@@ -33,6 +45,12 @@ public class ParamCoreSTReceiveActionBuilder extends STReceiveActionBuilder
 					.mapToObj(i -> ParamCoreSTApiGenConstants.GO_CROSS_RECEIVE_FUN_ARG
 							+ i + " *" + a.payload.elems.get(i)).collect(Collectors.joining(", "));
 	}
+
+	/*@Override
+	public String getReturnType(STStateChanApiBuilder api, EState curr, EState succ)
+	{
+		return api.cb.getCaseStateChanName(api, curr);
+	}*/
 
 	@Override
 	public String buildBody(STStateChanApiBuilder api, EState curr, EAction a, EState succ)
@@ -80,6 +98,7 @@ public class ParamCoreSTReceiveActionBuilder extends STReceiveActionBuilder
 				/*+ "if " + sEpErr + " != nil {\n"
 				+ "return nil\n"
 				+ "}\n"*/
-				buildReturn(api, curr, succ);
+				//buildReturn(api, curr, succ);
+				"return s._" + a.mid + "_Chan";
 	}
 }
