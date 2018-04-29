0981da24d98dee168716fa7fedf7dd7ffaa83cee
Ray Hu <raymond.hu@imperial.ac.uk>
2018-04-29 16:36:53 +0100

    [rp-core] Continued adding foreach.

---
+++ scribble-go/src/main/java/org/scribble/ext/go/util/RecursiveFunctionalInterface.java
@@ -0,0 +1,7 @@
+package org.scribble.ext.go.util;
+
+// Duplicated from org.scribble.ext.assrt.util
+public class RecursiveFunctionalInterface<F>  // F should be a functional interface
+{
+	public F func;
+}
