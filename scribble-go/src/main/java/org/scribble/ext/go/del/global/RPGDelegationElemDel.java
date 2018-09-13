9487aa28d3bf7405a6860ade2ace72d4ce1a61ba
Ray Hu <raymond.hu@imperial.ac.uk>
2018-09-13 19:45:12 +0100

    Continued.

---
+++ scribble-go/src/main/java/org/scribble/ext/go/del/global/RPGDelegationElemDel.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright 2008 The Scribble Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.scribble.ext.go.del.global;
+
+import org.scribble.ast.ProtocolDecl;
+import org.scribble.ast.context.ModuleContext;
+import org.scribble.ast.global.GDelegationElem;
+import org.scribble.ast.name.qualified.GProtocolNameNode;
+import org.scribble.del.global.GDelegationElemDel;
+import org.scribble.ext.go.ast.global.RPGDelegationElem;
+import org.scribble.main.ScribbleException;
+import org.scribble.type.kind.Global;
+import org.scribble.type.name.GProtocolName;
+import org.scribble.type.name.Role;
+import org.scribble.visit.wf.NameDisambiguator;
+
+public class RPGDelegationElemDel extends GDelegationElemDel
+{
+	public RPGDelegationElemDel()
+	{
+	
+	}
+
+	// Duplicated from super
+	@Override
+	public RPGDelegationElem visitForNameDisambiguation(NameDisambiguator disamb, GDelegationElem de) throws ScribbleException
+	{
+		ModuleContext mc = disamb.getModuleContext();
+		GProtocolName fullname = (GProtocolName) mc.getVisibleProtocolDeclFullName(de.proto.toName());
+
+		Role rn = de.role.toName();
+		ProtocolDecl<Global> gpd = disamb.job.getContext().getModule(fullname.getPrefix()).getProtocolDecl(fullname.getSimpleName());
+		if (!gpd.header.roledecls.getRoles().contains(rn))
+		{
+			String tmp = rn.toString();  // FIXME HACK (currently rely on checking valid variant later)
+			if (tmp.indexOf("_") == -1)
+			{
+				throw new ScribbleException(de.role.getSource(), "Invalid delegation role: " + de);
+			}
+		}
+
+		GProtocolNameNode pnn = (GProtocolNameNode) disamb.job.af.QualifiedNameNode(de.proto.getSource(), fullname.getKind(), fullname.getElements());  // Not keeping original namenode del
+		return (RPGDelegationElem) de.reconstruct(pnn, de.role);
+	}
+}
