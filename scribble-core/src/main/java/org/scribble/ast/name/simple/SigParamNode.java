214940129f0f45a76406860bc04e45ae64ac145a
Ray Hu <raymond.hu@imperial.ac.uk>
2019-03-17 09:55:39 +0000

    Continued: protocol header params refactoring.

---
+++ scribble-core/src/main/java/org/scribble/ast/name/simple/SigParamNode.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright 2008 The Scribble Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.scribble.ast.name.simple;
+
+import org.antlr.runtime.Token;
+import org.scribble.ast.AstFactory;
+import org.scribble.ast.MessageNode;
+import org.scribble.ast.MessageSigNode;
+import org.scribble.type.kind.SigKind;
+import org.scribble.type.name.MessageSigName;
+import org.scribble.visit.Substitutor;
+
+public class SigParamNode extends NonRoleParamNode<SigKind>
+		implements MessageNode
+{
+	// ScribTreeAdaptor#create constructor
+	public SigParamNode(Token t)
+	{
+		super(t, SigKind.KIND);
+	}
+
+	// Tree#dupNode constructor
+	protected SigParamNode(SigParamNode node)//, String id)
+	{
+		super(node);
+	}
+	
+	@Override
+	public SigParamNode dupNode()
+	{
+		if (getChildren().isEmpty())
+		{
+			throw new RuntimeException();
+		}
+		return new SigParamNode(this);//, getIdentifier());
+	}
+	
+	@Override
+	public SigParamNode project(AstFactory af)  // MessageSigName params
+	{
+		return this;
+	}
+	
+	@Override
+	public MessageSigNode substituteNames(Substitutor subs)
+	{
+		MessageSigName arg = toArg();
+		MessageSigNode an;
+		an = (MessageSigNode) subs.getArgumentSubstitution(arg);  // getArgumentSubstitution returns a clone
+		an = (MessageSigNode) an.del(del());
+		return an;
+	}
+	
+	@Override
+	public MessageSigName toName()
+	{
+		return new MessageSigName(getText());
+	}
+
+	@Override
+	public MessageSigName toArg()
+	{
+		return toMessage();
+	}
+
+	@Override
+	public MessageSigName toMessage()
+	{
+		return toName();
+	}
+
+	@Override
+	public boolean isTypeParamNode()
+	{
+		return true;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		int hash = 8609;
+		hash = 31 * super.hashCode();
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		if (this == o)
+		{
+			return true;
+		}
+		if (!(o instanceof SigParamNode))
+		{
+			return false;
+		}
+		return super.equals(o);
+	}
+	
+	@Override
+	public boolean canEqual(Object o)
+	{
+		return o instanceof SigParamNode;
+	}
+}
